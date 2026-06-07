17bc6a7aec6765ca9e592b8bff479ad565bbfa24
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-06-07 18:40:08 -0500

    add Log

---
+++ PlaneFiles/Core/LogWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class LogWriter:IDisposable
+    {
+        private readonly StreamWriter _writer;
+        public LogWriter(string path)
+        {
+            _writer = new StreamWriter(path, append: true)
+            {
+                AutoFlush = true
+            };
+            
+        }
+
+        public void WriteLog(string level, string messege)
+        {
+            var timeStamp = DateTime.Now.ToString("s");//ISO 8601 format
+            var logEntry = $"{timeStamp}[{level}]{messege}";
+            _writer.WriteLine(logEntry);
+        }
+
+        public void Dispose() 
+        {
+            _writer.Dispose();
+        }
+    }
+}
--- PlaneFiles/UI/Program.cs
+++ PlaneFiles/UI/Program.cs
@@ -1,63 +1,74 @@
 ﻿using Core;
 using System.ComponentModel.Design;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var textFile = new SimpleTextFile("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\Animals");
-var lines = textFile.ReadLines();
-var list=lines.ToList();
-var option = string.Empty;
-
-do
+using var logger = new LogWriter("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\app.log");
+try
 {
-    option = Menu();
-    switch (option)
+    logger.WriteLog("info", "Application started");
+    var lines = textFile.ReadLines();
+    var list = lines.ToList();
+    var option = string.Empty;
+
+    do
     {
-        case "1":
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-            break;
-        case "2":
-            {
+        option = Menu();
+        switch (option)
+        {
+            case "1":
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+                logger.WriteLog("info", "File listed");
+                break;
+            case "2":
                 Console.WriteLine("Enter a new line: ");
                 var newLine = Console.ReadLine();
                 list.Add(newLine!);
-            }
-            break;
-        case "3":
-            {
+                logger.WriteLog("info", $"New line added: {newLine} ");
+                break;
+            case "3":
                 Console.WriteLine("Enter a new line: ");
                 var lineToRemove = Console.ReadLine();
                 list.Remove(lineToRemove!);
-            }
-            break;
-        case "4":
-            {
+                logger.WriteLog("info", $"Line remove: {lineToRemove} ");
+                break;
+            case "4":
                 Console.WriteLine("Enter a new line: ");
-                var lineToRemove = Console.ReadLine();
+                var linesToRemove = Console.ReadLine();
+                list.RemoveAll(item => item.Equals(linesToRemove));
+                logger.WriteLog("info", $"All lines remove wiht value: {linesToRemove}");
 
-                list.RemoveAll(item => item.Equals(lineToRemove));
-                
-            }
-            break;
-        case "5":
-            {
+                break;
+            case "5":
                 list.Sort();
+                logger.WriteLog("info", "File saved");
+                break;
+            case "6":
+                {
+                    textFile.WriteLines(list.ToArray());
+                    Console.WriteLine("Changes saved");
+                }
+                break;
+            default:
+                break;
+        }
+    } while (option != "0");
+    textFile.WriteLines(list.ToArray());
+    Console.WriteLine("Changes saved");
 
-            }
-            break;
-        case "6":
-            {
-                textFile.WriteLines(list.ToArray());
-                Console.WriteLine("Changes saved");
-            }
-            break;
-        default:
-            break;
-    }
-} while (option != "0");
-textFile.WriteLines(list.ToArray());
-Console.WriteLine("Changes saved");
+}
+catch (Exception ex)
+{
+    logger.WriteLog("Error", $"An error happended: {ex.Message}.");
+}
+finally
+{
+    logger.WriteLog("info", "Application ended");
+}
+
 
 string Menu()
 {
