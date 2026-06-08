a943bff6db25dda12bdbf8f1b07594afd39c5bd8
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-06-07 19:08:41 -0500

    cvs manual

--- PlaneFiles/UI/Program.cs
+++ PlaneFiles/UI/Program.cs
@@ -1,86 +1,104 @@
-﻿using Core;
-using System.ComponentModel.Design;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿//using Core;
+//using System.ComponentModel.Design;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
 
-var textFile = new SimpleTextFile("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\Animals");
-using var logger = new LogWriter("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\app.log");
-try
+//var textFile = new SimpleTextFile("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\Animals");
+//using var logger = new LogWriter("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\app.log");
+//try
+//{
+//    logger.WriteLog("info", "Application started");
+//    var lines = textFile.ReadLines();
+//    var list = lines.ToList();
+//    var option = string.Empty;
+
+//    do
+//    {
+//        option = Menu();
+//        switch (option)
+//        {
+//            case "1":
+//                foreach (var item in list)
+//                {
+//                    Console.WriteLine(item);
+//                }
+//                logger.WriteLog("info", "File listed");
+//                break;
+//            case "2":
+//                Console.WriteLine("Enter a new line: ");
+//                var newLine = Console.ReadLine();
+//                list.Add(newLine!);
+//                logger.WriteLog("info", $"New line added: {newLine} ");
+//                break;
+//            case "3":
+//                Console.WriteLine("Enter a new line: ");
+//                var lineToRemove = Console.ReadLine();
+//                list.Remove(lineToRemove!);
+//                logger.WriteLog("info", $"Line remove: {lineToRemove} ");
+//                break;
+//            case "4":
+//                Console.WriteLine("Enter a new line: ");
+//                var linesToRemove = Console.ReadLine();
+//                list.RemoveAll(item => item.Equals(linesToRemove));
+//                logger.WriteLog("info", $"All lines remove wiht value: {linesToRemove}");
+
+//                break;
+//            case "5":
+//                list.Sort();
+//                logger.WriteLog("info", "File saved");
+//                break;
+//            case "6":
+//                {
+//                    textFile.WriteLines(list.ToArray());
+//                    Console.WriteLine("Changes saved");
+//                }
+//                break;
+//            default:
+//                break;
+//        }
+//    } while (option != "0");
+//    textFile.WriteLines(list.ToArray());
+//    Console.WriteLine("Changes saved");
+
+//}
+//catch (Exception ex)
+//{
+//    logger.WriteLog("Error", $"An error happended: {ex.Message}.");
+//}
+//finally
+//{
+//    logger.WriteLog("info", "Application ended");
+//}
+
+
+//string Menu()
+//{
+//    Console.WriteLine("Menu");
+//    Console.WriteLine("1. Show lines");
+//    Console.WriteLine("2. Add line");
+//    Console.WriteLine("3. Remove one occurrence");
+//    Console.WriteLine("4. Remove all occurrence");
+//    Console.WriteLine("5. Sort");
+//    Console.WriteLine("6. Save changes");
+//    Console.WriteLine("0. Exit");
+//    Console.WriteLine("Choose an option: ");
+//    return Console.ReadLine() ?? string.Empty;
+//}
+
+using Core;
+
+var people = new List<string[]>
 {
-    logger.WriteLog("info", "Application started");
-    var lines = textFile.ReadLines();
-    var list = lines.ToList();
-    var option = string.Empty;
+    new[] {"Id","Name","Age"},
+    new[] {"1","Juan","51"},
+    new[] {"2","Carlos","27"},
+    new[] {"3","Emma","0"},
+};
 
-    do
-    {
-        option = Menu();
-        switch (option)
-        {
-            case "1":
-                foreach (var item in list)
-                {
-                    Console.WriteLine(item);
-                }
-                logger.WriteLog("info", "File listed");
-                break;
-            case "2":
-                Console.WriteLine("Enter a new line: ");
-                var newLine = Console.ReadLine();
-                list.Add(newLine!);
-                logger.WriteLog("info", $"New line added: {newLine} ");
-                break;
-            case "3":
-                Console.WriteLine("Enter a new line: ");
-                var lineToRemove = Console.ReadLine();
-                list.Remove(lineToRemove!);
-                logger.WriteLog("info", $"Line remove: {lineToRemove} ");
-                break;
-            case "4":
-                Console.WriteLine("Enter a new line: ");
-                var linesToRemove = Console.ReadLine();
-                list.RemoveAll(item => item.Equals(linesToRemove));
-                logger.WriteLog("info", $"All lines remove wiht value: {linesToRemove}");
+var manualCvsHelper = new ManualCvsHelper();
+manualCvsHelper.WriteCSV("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\people.csv", people);
 
-                break;
-            case "5":
-                list.Sort();
-                logger.WriteLog("info", "File saved");
-                break;
-            case "6":
-                {
-                    textFile.WriteLines(list.ToArray());
-                    Console.WriteLine("Changes saved");
-                }
-                break;
-            default:
-                break;
-        }
-    } while (option != "0");
-    textFile.WriteLines(list.ToArray());
-    Console.WriteLine("Changes saved");
-
-}
-catch (Exception ex)
+var loadedPeople = manualCvsHelper.ReadCSV("C:\\Users\\Carlos Gonzalez\\Documents\\cursos\\Estructura de Datos\\tmp\\people.csv");
+foreach(var person in loadedPeople)
 {
-    logger.WriteLog("Error", $"An error happended: {ex.Message}.");
+    Console.WriteLine(string.Join("|", person));
 }
-finally
-{
-    logger.WriteLog("info", "Application ended");
-}
-
-
-string Menu()
-{
-    Console.WriteLine("Menu");
-    Console.WriteLine("1. Show lines");
-    Console.WriteLine("2. Add line");
-    Console.WriteLine("3. Remove one occurrence");
-    Console.WriteLine("4. Remove all occurrence");
-    Console.WriteLine("5. Sort");
-    Console.WriteLine("6. Save changes");
-    Console.WriteLine("0. Exit");
-    Console.WriteLine("Choose an option: ");
-    return Console.ReadLine() ?? string.Empty;
-}
-
